8d7c06b3fb942242cfcdf493c0c41bf3e2c24bd7
Einsam <snitxmhm@gmail.com>
2022-03-31 21:32:48 +0800

    some update

--- b/rclone.sh
+++ b/rclone.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rclone serve webdav /home/einsam/d/books --addr 0.0.0.0:8081 &
-rclone serve webdav /home/einsam/m/v --addr 0.0.0.0:8080 &
+rclone serve webdav /home/einsam/d/books --addr 0.0.0.0:9998 &
+rclone serve webdav /home/einsam/m/v --addr 0.0.0.0:9999 &
 
