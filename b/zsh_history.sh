0fa7583dfc046b6c0b278d62ac739a7af24dc983
Einsam <snitxmhm@gmail.com>
2022-10-27 11:28:09 +0800

    update: zsh_history.sh

--- b/zsh_history.sh
+++ b/zsh_history.sh
@@ -3,4 +3,5 @@
 cp ~/.local/state/zsh/.histfile ~/p/backup/zsh/.histfile
 cd ~/p/backup/zsh
 cat zsh_history_$(date +\%Y_\%m_\%d_)*.bak .histfile .hist | sort | uniq > .hist
-cp .hist ~/.local/state/zsh/.histfile
+rg --invert-match '0;(ls|tldr|phone|cd|lg|ra|s|c|la|ll|lvim|history|ef|vf|zsh|e|fzf|\\|evim|vim|make|colemak|\.\.|\.|cd \-|hi|zsh_history\.sh)$' .hist > .hist1
+cp .hist1 ~/.local/state/zsh/.histfile
